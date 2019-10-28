1369a7ff9c7221ad2ba19698a8ce927637b9fc51
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-10-28 23:11:29 +0100

    Styles on desktop

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import date from "./components/date";
 import navigation from "./components/navigation";
+import form from './components/form';
 
 $(function() {
     date();
     navigation();
+    form();
 });
--- src/js/components/date.js
+++ src/js/components/date.js
@@ -1,4 +1,4 @@
 export default function date() {
     const $year =$('.footer__date');
-    $year.innerHTML = new Date().getFullYear();
+    $year.text((new Date).getFullYear());
 }
