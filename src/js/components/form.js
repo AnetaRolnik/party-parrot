dced0da87ebd2d99835c6ea45b9d9a576048bf3b
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-10-29 23:08:27 +0100

    Favicon, contact form and small fixes

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,2 +1,9 @@
 export default function form() {
+    const $fields = $('.page__element');
+
+    $fields.each(function () {
+        $(this).on("change", function () {
+            $(this).val().length > 0 ?  $(this).addClass('active') : $(this).removeClass('active');
+        });
+    });
 }
