6dc7461cabdd08cb540dbd2603a37f9ce41fdf9c
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-11-03 20:15:10 +0100

    Close hamburger menu after clicking link

--- src/js/components/links.js
+++ src/js/components/links.js
@@ -1,10 +1,13 @@
 export default function links() {
-    const $links = $('.nav__link');
+    const $links = $('.nav__link'),
+        $body = $('.page');
 
     $links.each(function () {
         $(this).on("click", function () {
             $links.removeClass('active');
             $(this).addClass('active');
+            backdrop.remove();
+            $body.removeClass('hamburger--open');
         });
     });
 }
